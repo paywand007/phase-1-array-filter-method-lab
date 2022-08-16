cc9315ee62b43e5c7b09ba8610c4ca121720490d
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-08-16 17:15:29 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(paiwand, sought) {
+    return paiwand.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+        (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+}
